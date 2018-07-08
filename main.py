13befe879d92ae53248ac07b430f7d0dd3cdabd4
Marani Matias Ezequiel <maranimatias@gmail.com>
2018-07-07 23:56:03 -0300

    Add other metric

--- main.py
+++ main.py
@@ -1,18 +1,28 @@
-import os
 from keras.applications.inception_v3 import InceptionV3
-# from keras.preprocessing import image
-from keras.models import Model
-from keras.layers import Flatten, Dense, Input
 # from keras import backend as K
-import keras
+from keras.layers import Flatten, Dense, Input
+from keras.models import Model
+from keras.optimizers import Adam, RMSprop
+# from keras.preprocessing import image
 from six.moves import cPickle
+import keras
+import matplotlib.pyplot as plt
 import numpy as np
-# import matplotlib.pyplot as plt
+import os
 
+np.random.seed(1671)  # for reproducibility
 __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
 
-batch_size = 32
-epochs = 3
+# network and training
+EPOCHS = 3
+BATCH_SIZE = 32
+VERBOSE = 1
+NB_CLASSES = 10   # number of outputs = number of digits
+# OPTIMIZER = Adam()
+OPTIMIZER = RMSprop()
+N_HIDDEN = 128
+VALIDATION_SPLIT = 0.2  # how much TRAIN is reserved for VALIDATION
+DROPOUT = 0.3
 
 # Load data
 print('...loading training data')
@@ -66,21 +76,54 @@ x = Dense(512, activation='relu')(x)
 predictions = Dense(1)(x)
 
 model = Model(inputs=input, outputs=predictions)
-model.compile(optimizer='adam', loss='mean_squared_error', metrics=['MAE'])
+
+model.compile(
+    optimizer=OPTIMIZER,
+    loss='categorical_crossentropy',
+    metrics=['MAE', 'accuracy']
+)
 
 # Save weights after every epoch
 checkpoint = keras.callbacks.ModelCheckpoint(
     filepath='weights/weights.{epoch:02d}-{val_loss:.2f}.hdf5',
     save_weights_only=True,
     period=1)
-history = model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs, verbose=1,
-                    validation_data=(x_valid, y_valid), callbacks=[checkpoint])
+
+history = model.fit(
+    x_train, y_train,
+    batch_size=BATCH_SIZE,
+    epochs=EPOCHS,
+    verbose=VERBOSE,
+    validation_split=VALIDATION_SPLIT,
+    validation_data=(x_valid, y_valid),
+    callbacks=[checkpoint]
+)
 
 model.save_weights("model.h5")
 
-score = model.evaluate(x_test, y_test, batch_size=batch_size)
-print('Test loss:', score[0])
+score = model.evaluate(x_test, y_test, batch_size=BATCH_SIZE, verbose=VERBOSE)
+print('\nTest loss:', score[0])
 print('Test MAE:', score[1])
+print('Test accuracy:', score[2])
+
+# list all data in history
+print(history.history.keys())
+# summarize history for accuracy
+plt.plot(history.history['acc'])
+plt.plot(history.history['val_acc'])
+plt.title('model accuracy')
+plt.ylabel('accuracy')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.show()
+# summarize history for loss
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.title('model loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.show()
 
 with open('history.pkl', 'wb') as f:
     cPickle.dump(history.history, f)
