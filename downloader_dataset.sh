ea1226c3e70f80ec7024a825ff2e6e393c8f8726
Marani Matias Ezequiel <maranimatias@gmail.com>
2018-08-03 09:24:59 -0300

    Delete dataset.zip after downloaded

--- downloader_dataset.sh
+++ downloader_dataset.sh
@@ -19,4 +19,4 @@ set -o nounset                              # Treat unset variables as an error
 # Dataset 209 img, 106 male and 103 famele
 dataset="https://doc-0k-4c-docs.googleusercontent.com/docs/securesc/ha0ro937gcuc7l7deffksulhg5h7mbp1/p6j0socede0t3ep0q92gfprjogi786v9/1533290400000/03448145893575284703/*/1DeMVpdFLya8IOAyP31tv6WXr5Kn-rVcG?e=download"
 
-curl $(echo "$dataset") -o dataset.zip && unzip dataset.zip
+curl $(echo "$dataset") -o dataset.zip && unzip dataset.zip && rm dataset.zip
