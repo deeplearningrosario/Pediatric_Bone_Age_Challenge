a4c9b597d920bdaa66ef842201ac05c3c3138fdd
Marani Matias Ezequiel <maranimatias@gmail.com>
2018-08-14 22:21:47 -0300

    Reading images according to the CSV order.

--- pickle_dataset.py
+++ pickle_dataset.py
@@ -1,29 +1,22 @@
 #!/usr/bin/python3
-
-from deep_cut_hand.main_histogram import makerModel, getHistogram
 from six.moves import cPickle
 import cv2
-import fnmatch
 import math
 import numpy as np
 import os
 import pandas as pd
+import h5py
 import sys
 
-# Directory of dataset to use
+# TODO Directory of dataset to use
 TRAIN_DIR = "dataset_sample"
 # TRAIN_DIR = "boneage-training-dataset"
 
 # Use N images of dataset, If it is -1 using all dataset
-CUT_DATASET = 1000
-# Sort dataset randomly
-SORT_RANDOMLY = True
+CUT_DATASET = -1
 
-# Separate data set by gender
-# male, female, both
-SPLIT_GENDER = 'both'
-# SPLIT_GENDER = 'female'
-# SPLIT_GENDER = 'male'
+# Remove images that are less than or equal to 23 months of age
+REMOVE_AGE = 23
 
 # Turn saving renders feature on/off
 SAVE_RENDERS = False
@@ -36,25 +29,18 @@ SAVE_IMAGE_FOR_DEBUGGER = False
 # Simple dataset is correct, I am verifying the original.
 EXTRACTING_HANDS = True
 
-# Using deep learning for normalizing histogram of level colors
-IA_EXTRACTING_HANDS = True
-
 # Turn rotate image on/off
-ROTATE_IMAGE = False
+ROTATE_IMAGE = True
 
 # For this problem the validation and test data provided by the concerned authority did not have labels,
 # so the training data was split into train, test and validation sets
 __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
-
 train_dir = os.path.join(__location__, TRAIN_DIR)
-
 img_file = ""
 
-# Save model
-deep_model = None
-
-
 # Show the images
+
+
 def writeImage(path, image, force=False):
     if SAVE_IMAGE_FOR_DEBUGGER or force:
         cv2.imwrite(os.path.join(__location__, TRAIN_DIR, path, img_file), image)
@@ -213,21 +199,10 @@ def updateProgress(progress, tick="", total="", status="Loading..."):
 
 
 def getColorsHands(img):
-    if IA_EXTRACTING_HANDS == True:
-        X_img = getHistogram(img)
-        X_new = np.array([X_img])
-
-        predict = deep_model.predict(X_new)
-        min_color = int(predict[0][0])
-        max_color = int(predict[1][0])
-        min_color = min_color if min_color > 0 else 0
-        max_color = max_color if max_color < 255 else 255
-        # print("Lower: %s, Upper: %s" % (min_color, max_color))
-    else:
-        # Find the acceptable limits of the intensity histogram
-        min_color, max_color = np.percentile(img, (2.5, 99.4))
-        min_color = int(min_color)
-        max_color = int(max_color)
+    # Find the acceptable limits of the intensity histogram
+    min_color, max_color = np.percentile(img, (2.5, 99.4))
+    min_color = int(min_color)
+    max_color = int(max_color)
 
     return (min_color, max_color)
 
@@ -262,34 +237,25 @@ def processImage(img_path):
 
 
 def loadDataSet(files=[]):
-    global deep_model
     global img_file
     X_train = []
     y_age = []
     y_gender = []
 
-    if IA_EXTRACTING_HANDS == True:
-        deep_model = makerModel()
-        try:
-            deep_model.load_weights(os.path.join(
-                __location__, "deep_cut_hand", "model", "model_histogram.h5"))
-        except:
-            print("I can not find the weights for the model.")
-            exit(0)
-
     total_file = len(files)
     for i in range(total_file):
         (img_file, bone_age, gender) = files[i]
+        img_file = str(img_file) + ".png"
         # Update the progress bar
         progress = float(i / total_file), (i + 1)
         updateProgress(progress[0], progress[1], total_file, img_file)
 
         # Get image's path
         img_path = os.path.join(train_dir, img_file)
-
-        X_train.append(processImage(img_path))
-        y_gender.append(gender)
-        y_age.append(bone_age)
+        if os.path.exists(img_path):
+            X_train.append(processImage(img_path))
+            y_gender.append(gender)
+            y_age.append(bone_age)
 
     updateProgress(1, total_file, total_file, img_file)
 
@@ -297,64 +263,54 @@ def loadDataSet(files=[]):
 
 
 # Save dataset
-def saveDataSet(X_train, y_age, y_gender):
-    print("\nSaving data...", "\nGender to use %s " % (SPLIT_GENDER))
-    # Save data
-    train_pkl = open("data.pkl", "wb")
-    cPickle.dump(X_train, train_pkl, protocol=cPickle.HIGHEST_PROTOCOL)
-    train_pkl.close()
-
-    train_age_pkl = open("data_age.pkl", "wb")
-    cPickle.dump(y_age, train_age_pkl, protocol=cPickle.HIGHEST_PROTOCOL)
-    train_age_pkl.close()
-
-    train_gender_pkl = open("data_gender.pkl", "wb")
-    cPickle.dump(y_gender, train_gender_pkl, protocol=cPickle.HIGHEST_PROTOCOL)
-    train_gender_pkl.close()
-    print("Completed saved data")
+def saveDataSet(gender, X_train, y_age, y_gender):
+    print("\nSaving data...")
+    with h5py.File(
+        os.path.join(train_dir, "packaging-dataset", "dataset.hdf5"), "w"
+    ) as f:
+        f.create_dataset(
+            "img", data=X_train, dtype=np.float, compression="gzip", compression_opts=5
+        )
+        f.create_dataset("age", data=y_age)
+        f.create_dataset("gender", data=y_gender)
+        f.flush()
+        f.close()
 
 
 # list all the image files and randomly unravel them,
 # in each case you take the first N from the unordered list
 def getFiles():
-    rta = []
+    print("Read csv on", TRAIN_DIR)
+    famale = []
+    male = []
     # Read csv
     df = pd.read_csv(os.path.join(train_dir, "boneage-training-dataset.csv"))
 
-    # file names on train_dir
-    files = os.listdir(train_dir)
-    # filter image files
-    files = [f for f in files if fnmatch.fnmatch(f, "*.png")]
-    # Sort randomly
-    if SORT_RANDOMLY:
-        np.random.shuffle(files)
-
-    for file_name in files:
+    for index, row in df.iterrows():
         # Cut list of file
-        if CUT_DATASET <= 0 or len(rta) < CUT_DATASET:
-            # Get row with id equil image's name
-            csv_row = df[df.id == int(file_name[:-4])]
+        if CUT_DATASET <= 0 or (len(famale) + len(male)) < CUT_DATASET:
             # Get bone age
-            bone_age = csv_row.boneage.tolist()[0]
-            # Get gender
-            male = csv_row.male.tolist()[0]
+            bone_age = row.boneage
+            if bone_age > REMOVE_AGE:
+                # Get gender
+                if row.male:
+                    male.append((row.id, bone_age, 1))
+                else:
+                    famale.append((row.id, bone_age, 0))
 
-            if SPLIT_GENDER == 'female' and not(male):
-                rta.append((file_name, bone_age, 1 if male else 0))
-            if SPLIT_GENDER == 'male' and male:
-                rta.append((file_name, bone_age, 1 if male else 0))
-            if SPLIT_GENDER == 'both':
-                rta.append((file_name, bone_age, 1 if male else 0))
-    return rta
+    return famale, male
 
 
 # Create the directories to save the images
 def checkPath():
+    if not os.path.exists(os.path.join(__location__, "packaging-dataset")):
+        os.makedirs(os.path.join(__location__, "packaging-dataset"))
+
     if SAVE_IMAGE_FOR_DEBUGGER:
-        for folder in ["histograms_level_fix", "cut_hand", "render", "mask"]:
+        for folder in ["histograms_level_fix", "cut_hand", "mask"]:
             if not os.path.exists(os.path.join(__location__, TRAIN_DIR, folder)):
                 os.makedirs(os.path.join(__location__, TRAIN_DIR, folder))
-    if SAVE_RENDERS:
+    if SAVE_RENDERS or SAVE_IMAGE_FOR_DEBUGGER:
         if not os.path.exists(os.path.join(__location__, TRAIN_DIR, "render")):
             os.makedirs(os.path.join(__location__, TRAIN_DIR, "render"))
 
@@ -366,6 +322,14 @@ def checkPath():
 if __name__ == "__main__":
     checkPath()
 
-    files = getFiles()
-    (X_train, y_age, y_gender) = loadDataSet(files)
-    saveDataSet(X_train, y_age, y_gender)
+    famale, male = getFiles()
+
+    print("Processing female images...")
+    (X_train, y_age, y_gender) = loadDataSet(famale)
+    print(len(famale), len(X_train))
+    # saveDataSet(famale,X_train, y_age, y_gender)
+
+    print("Processing male images...")
+    (X_train, y_age, y_gender) = loadDataSet(male)
+    print(len(male), len(X_train))
+    # saveDataSet(male,X_train, y_age, y_gender)
