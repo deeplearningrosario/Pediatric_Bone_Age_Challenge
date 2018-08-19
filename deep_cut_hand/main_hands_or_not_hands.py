47e65468720ded0c56470acd866e5f412756bec5
Marani Matias Ezequiel <maranimatias@gmail.com>
2018-08-19 17:59:56 -0300

    Add model hands not hands

---
+++ deep_cut_hand/main_hands_or_not_hands.py
@@ -0,0 +1,230 @@
+#!/usr/bin/python3
+# ./main_histogram.py -lw ./model/model_histogram.h5
+# ./main_histogram.py -lw ./model/model_histogram.h5 --train False
+# ./main_histogram.py -lw ./model/model_histogram.h5 --train False --evaluate True --predict True
+
+from keras.layers import Flatten, Dense, Input, Dropout, BatchNormalization, concatenate
+from keras.models import Model
+from keras.optimizers import Adam, RMSprop, Adadelta, Adagrad
+from utilities import Console
+import h5py
+import argparse
+import keras
+import numpy as np
+import os
+
+# network and training
+EPOCHS = 300
+BATCH_SIZE = 22
+
+# https://keras.io/optimizers
+# OPT = Adam(lr=0.001)
+OPT = RMSprop()
+# OPT = Adadelta(lr=0.01, rho=0.95, epsilon=None, decay=0.0)
+# OPT = Adagrad(lr=0.05)
+
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-lw", "--load_weights", help="Path to the file weights")
+ap.add_argument("-tb", "--tensorBoard", default="False", help="Active tensorBoard")
+ap.add_argument("-cp", "--checkpoint", default="False", help="Active checkpoint")
+ap.add_argument(
+    "-rl", "--reduce_learning", default="False", help="Active reduce learning rate"
+)
+
+ap.add_argument("-t", "--train", default="True", help="Run train model")
+ap.add_argument("-e", "--evaluate", default="False", help="Evaluating model")
+ap.add_argument("-p", "--predict", help="File to predict values")
+args = vars(ap.parse_args())
+
+# For this problem the validation and test data provided by the concerned authority did not have labels,
+# so the training data was split into train, test and validation sets
+__location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+# Turn saving renders feature on/off
+SAVE_RENDERS = False
+
+# Create intermediate images in separate folders for debugger.
+# mask, cut_hand, delete_object, render
+SAVE_IMAGE_FOR_DEBUGGER = False
+
+
+# list all the image files and randomly unravel them,
+# in each case you take the first N from the unordered list
+def getDatasetFile():
+    dataset_file = os.path.join(__location__, "histogram-hand-dataset.hdf5")
+
+    with h5py.File(dataset_file, "r+") as f:
+        X_train = f["hist"][()]
+        y_train = f["valid"][()]
+        Console.log("Dataset file count", len(y_train))
+        f.close()
+
+    return X_train, y_train
+
+
+# Make Keras callback
+def loadCallBack():
+    cb = []
+
+    # TensorBoard
+    # how to use: $ tensorboard --logdir path_to_current_dir/Graph
+    if args["tensorBoard"] == "True":
+        # Save log for tensorboard
+        LOG_DIR_TENSORBOARD = os.path.join(__location__, "..", "tensorboard")
+        if not os.path.exists(LOG_DIR_TENSORBOARD):
+            os.makedirs(LOG_DIR_TENSORBOARD)
+
+        tbCallBack = keras.callbacks.TensorBoard(
+            log_dir=LOG_DIR_TENSORBOARD,
+            batch_size=BATCH_SIZE,
+            histogram_freq=0,
+            write_graph=True,
+            write_images=True,
+        )
+
+        Console.info("tensorboard --logdir", LOG_DIR_TENSORBOARD)
+        cb.append(tbCallBack)
+
+    if args["checkpoint"] == "True":
+        # Save weights after every epoch
+        if not os.path.exists(os.path.join(__location__, "weights")):
+            os.makedirs(os.path.join(__location__, "weights"))
+        checkpoint = keras.callbacks.ModelCheckpoint(
+            filepath="weights/weights.{epoch:02d}-{val_loss:.2f}.hdf5",
+            save_weights_only=True,
+            period=1,
+        )
+        Console.info("Save weights after every epoch")
+        cb.append(checkpoint)
+
+    # Reduce learning rate
+    if args["reduce_learning"] == "True":
+        reduceLROnPlat = keras.callbacks.ReduceLROnPlateau(
+            monitor="val_loss", factor=0.8, patience=3, verbose=1, min_lr=0.0001
+        )
+        Console.info("Add Reduce learning rate")
+        cb.append(reduceLROnPlat)
+
+    return cb
+
+
+def makerModel():
+    # First we need to create a model structure
+    hist_input = Input(shape=(256,), name="hist_input")
+
+    x = Dense(256, activation="sigmoid")(hist_input)
+    x = Dense(256, activation="relu")(x)
+    x = Dense(64, activation="relu")(x)
+
+    # Prediction for the upper and lower value
+    hands = Dense(1, activation="softmax", name="hands")(x)
+
+    model = Model(inputs=[hist_input], outputs=[hands])
+
+    model.compile(loss="mean_squared_error", metrics=["MAE", "accuracy"], optimizer=OPT)
+
+    if __name__ == "__main__":
+        Console.info("Model summary")
+        print(model.summary())
+
+    # Load weight
+    if args["load_weights"] != None:
+        Console.info("Loading weights from", args["load_weights"])
+        model.load_weights(args["load_weights"])
+
+    return model
+
+
+def trainModel(model, X_train, y_train):
+    Console.info("Create validation sets, training set, testing set...")
+    # Split images dataset
+    k = int(len(X_train) / 6)  # Decides split count
+
+    hist_test = X_train[:k]
+    hand_test = y_train[:k]
+
+    hist_valid = X_train[k: 2 * k]
+    hand_valid = y_train[k: 2 * k]
+
+    hist_train = X_train[2 * k:]
+    hand_train = y_train[2 * k:]
+
+    Console.info("Training network...")
+    history = model.fit(
+        [hist_train],
+        [hand_train],
+        batch_size=BATCH_SIZE,
+        epochs=EPOCHS,
+        verbose=2,
+        validation_data=([hist_valid], [hand_valid]),
+        callbacks=loadCallBack(),
+    )
+
+    Console.info("Save model to disck...")
+    # Path to save model
+    PATHE_SAVE_MODEL = os.path.join(__location__, "model")
+
+    # Save weights after every epoch
+    if not os.path.exists(PATHE_SAVE_MODEL):
+        os.makedirs(PATHE_SAVE_MODEL)
+
+    # serialize model to YAML
+    model_yaml = model.to_yaml()
+    with open(os.path.join(PATHE_SAVE_MODEL, "model_hands_not_hands.yaml"), "w") as yaml_file:
+        yaml_file.write(model_yaml)
+    # serialize weights to HDF5
+    model.save_weights(os.path.join(PATHE_SAVE_MODEL, "model_hands_not_hands.h5"))
+    print("OK")
+
+    # evaluate the network
+    Console.info("Evaluating network...")
+    score = model.evaluate(
+        [hist_test], [hand_test], batch_size=BATCH_SIZE, verbose=1
+    )
+
+    Console.log("Test loss:", score[0])
+    Console.log("Test MAE:", score[1])
+    Console.log("Test Acc:", score[2])
+
+    # list all data in history
+    Console.info("Save model history graphics...")
+    print(history.history.keys())
+
+
+# Como vamos a usar multi procesos uno por core.
+# Los procesos hijos cargan el mismo código.
+# Este if permite que solo se ejecute lo que sigue si es llamado
+# como proceso raíz.
+if __name__ == "__main__":
+    if args["predict"] == None or args["predict"] == "True":
+        (X_train, y_train) = getDatasetFile()
+
+    # Create model
+    model = makerModel()
+
+    if args["train"] == "True":
+        trainModel(model, X_train, y_train)
+
+    if args["evaluate"] == "True":
+        Console.info("Evaluating model...")
+        score = model.evaluate(
+            [X_train], [y_train], batch_size=BATCH_SIZE, verbose=1
+        )
+        Console.log("Test loss:", score[0])
+        Console.log("Test MAE:", score[1])
+        Console.log("Test Acc:", score[2])
+
+    if args["predict"] != None and args["predict"] != "False":
+        if args["predict"] != "True":
+            Console.info("Predict for", args["predict"])
+        else:
+            Console.info("Predict...")
+            # new instance where we do not know the answer
+            Xnew = X_train
+            Xnew = np.array(Xnew)
+
+            # make a prediction
+            ynew = model.predict(Xnew)
+
+            for y in ynew:
+                Console.log("y", y)
